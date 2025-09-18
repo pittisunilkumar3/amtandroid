965400ad83273d4bd8bb2be3b7402306afd4e5ee
pittisunilkumar3 <pittisunilkumar3@gmail.com>
2025-09-18 15:45:55 +0530

    updated  teacher dashboard

--- app/src/main/java/com/qdocs/ssre241123/teachers/TeacherDashboard.java
+++ app/src/main/java/com/qdocs/ssre241123/teachers/TeacherDashboard.java
@@ -18,15 +18,11 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
 import androidx.core.view.GravityCompat;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
@@ -66,13 +62,7 @@ public class TeacherDashboard extends AppCompatActivity implements NavigationVie
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_teacher_dashboard);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
         // Check if teacher is logged in
         if (!TeacherAuthHelper.isTeacherLoggedIn(this)) {
